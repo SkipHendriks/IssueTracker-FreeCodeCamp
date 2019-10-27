7642028495ec093c3ff55344bdd59908b8916bd1
Skip Hendriks <sigmar001@gmail.com>
2019-10-27 11:10:18 +1100

    Models draft

---
+++ src/models/issue.model.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IIssue extends Document {
+  issue_title: String,
+  issue_text: String,
+  created_by: String,
+  assigned_to: String,
+  open: String,
+  status_text: String
+}
+
+const IssueSchema: Schema = new Schema({
+  issue_title: { type: String, required: true},
+  issue_text: { type: String, required: true},
+  created_by: { type: String, required: true},
+  assigned_to: { type: String },
+  open: { type: Boolean, required: true, default: true },
+  status_text: { type: String },
+});
+
+export default mongoose.model<IIssue>('Issue', IssueSchema);
