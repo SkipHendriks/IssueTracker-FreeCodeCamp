979161269f4c206d06ac66a87988eeae6fecd166
Skip Hendriks <sigmar001@gmail.com>
2019-11-10 15:21:08 +1100

    React app config

---
+++ src/client/webpack.config.ts
@@ -0,0 +1,37 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import htmlWebpackTemplate from 'html-webpack-template';
+import webpack from 'webpack';
+
+const htmlOptions: htmlWebpackTemplate.Options = {
+  title: 'Issue Tracker | freeCodeCamp',
+  template: htmlWebpackTemplate,
+  inject: false,
+  appMountId: 'app',
+};
+
+const config: webpack.Configuration = {
+  mode: 'development',
+  watch: true,
+  entry: './index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: `${__dirname}/../../dist/public`,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+      { test: /\.tsx?$/, loader: 'babel-loader' },
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin(htmlOptions),
+  ],
+};
+
+export default config;
