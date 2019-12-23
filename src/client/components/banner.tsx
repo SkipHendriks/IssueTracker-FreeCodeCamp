d8b36e0e1730a1b05fcab29927a51552f2eebd35
Skip Hendriks <sigmar001@gmail.com>
2019-12-23 19:21:33 +0700

    Styled project dropdown

--- src/client/components/banner.tsx
+++ src/client/components/banner.tsx
@@ -1,12 +1,12 @@
-import * as React from 'react';
+import React from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import {
   AppBar,
   Toolbar,
   Typography,
-  FormControl,
   Select,
   MenuItem,
+  OutlinedInput,
 } from '@material-ui/core';
 import { IProject } from '../../models/project.model';
 
@@ -17,24 +17,49 @@ interface IProps {
   currentProject: string;
 }
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    formControl: {
-      margin: theme.spacing(1),
+const useOutlinedInputStyles = makeStyles((theme: Theme) => createStyles({
+  root: {
+    margin: theme.spacing(2),
       minWidth: 153,
+    color: theme.palette.primary.contrastText,
+    '& $notchedOutline': {
+      border: 0,
     },
-    selectText: {
-      paddingRight: theme.spacing(5),
+    '&:hover $notchedOutline': {
+      border: 0,
+    },
+    '&$focused $notchedOutline': {
+      border: 0,
+      borderRadius: 'inherit',
+    },
+  },
+  focused: {},
+  notchedOutline: {},
+}));
+
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  select: {
+    padding: `${theme.spacing(2)}px ${theme.spacing(5)}px ${theme.spacing(2)}px ${theme.spacing(2)}px`,
+    color: theme.palette.primary.contrastText,
+    backgroundColor: theme.palette.primary.light,
+    '&:focus': {
+      backgroundColor: theme.palette.primary.light,
+      borderRadius: 'inherit',
+    },
     },
     title: {
       flexGrow: 1,
     },
+  selectIcon: {
+    display: 'none',
+  },
   }));
 
 export default ({
   projects, isLoading, handleChange, currentProject,
 }: IProps) => {
   const classes = useStyles({});
+  const outlinedInputClasses = useOutlinedInputStyles({});
 
   let menuItems;
   if (isLoading) {
@@ -74,7 +99,14 @@ export default ({
           <Select
             onChange={handleChange}
             value={selectValue}
-            classes={{ root: classes.selectText }}
+          variant="outlined"
+          classes={{ root: classes.select, icon: classes.selectIcon }}
+          input={(
+            <OutlinedInput
+              labelWidth={0}
+              classes={outlinedInputClasses}
+            />
+          )}
           >
             { menuItems }
           </Select>
