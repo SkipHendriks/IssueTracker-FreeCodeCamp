9144beb28661ecb87ac97f6d70a5ae58dbea3db1
Skip Hendriks <sigmar001@gmail.com>
2020-01-02 16:56:28 +0700

    Added add button

--- src/client/components/issue-container.tsx
+++ src/client/components/issue-container.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { Container, LinearProgress } from '@material-ui/core';
-import { withStyles, WithStyles, Theme } from '@material-ui/core/styles';
+import { Container, LinearProgress, Fab } from '@material-ui/core';
+import {
+  createStyles, withStyles, WithStyles, Theme,
+} from '@material-ui/core/styles';
+import { Add as AddIcon } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
 
 
 import Issue from './issue';
 import { IProject } from '../../models/project.model';
 import { IIssue } from '../../models/issue.model';
 
-const styles = (theme: Theme) => ({
-  root: {
+const styles = (theme: Theme) => createStyles({
+  mainContainer: {
     paddingTop: theme.spacing(2),
     paddingBottom: theme.spacing(2),
     backgroundColor: '#f5f5f6',
     minHeight: 'calc(100vh - 72px)',
+  },
+  addButton: {
+    margin: 0,
+    top: 'auto',
+    right: 20,
+    bottom: 20,
+    left: 'auto',
+    position: 'fixed',
   },
 });
 
@@ -52,15 +64,21 @@ class IssueContainer extends React.Component<IProps> {
 
   render() {
     const { issues, isLoading } = this.state;
-    const { classes } = this.props;
+    const { classes, currentProject } = this.props;
+    const addUrl = currentProject ? `${currentProject.name}/add` : '/add';
     return (
       <>
         {isLoading && <LinearProgress />}
         {!isLoading && (
-          <Container className={classes.root}>
-              {!isLoading && issues.map((issue: IIssue) => (
-                <Issue issue={issue} key={issue._id} />
-              ))}
+          <Container className={classes.mainContainer}>
+            {!isLoading && issues.map((issue: IIssue) => (
+              <Issue issue={issue} key={issue._id} />
+            ))}
+            <Link to={addUrl}>
+              <Fab color="primary" className={classes.addButton}>
+                <AddIcon />
+              </Fab>
+            </Link>
           </Container>
         )}
       </>
