065c9fcfa23b94722591cad4372ef484a48bed49
Skip Hendriks <sigmar001@gmail.com>
2020-01-02 16:58:44 +0700

    Removed SCSS import

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -5,7 +5,6 @@ import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/app';
-// import './styles/global.scss';
 
 ReactDOM.render(
   <BrowserRouter>
